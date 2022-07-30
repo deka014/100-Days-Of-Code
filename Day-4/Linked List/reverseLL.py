5a35bd085c753693eda1a2200989e4ead085962e
deka014 <deepjyotideka8@gmail.com>
2022-07-30 21:30:41 +0530

    striver linked list question

---
+++ Day-4/Linked List/reverseLL.py
@@ -0,0 +1,36 @@
+# Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+# Input: head = [1,2,3,4,5]
+# Output: [5,4,3,2,1]
+
+# Input: head = [1,2]
+# Output: [2,1]
+
+# https://leetcode.com/problems/reverse-linked-list/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        resultNode = None
+        
+        while head != None : 
+            
+            
+            nextNode = head.next
+            
+            currentNode = head
+            currentNode.next = resultNode
+            resultNode = currentNode
+            
+            head = nextNode
+        
+        return resultNode
+
+
