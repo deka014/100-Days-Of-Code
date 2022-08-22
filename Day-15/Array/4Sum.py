be5c57d3bc52f0e3c5571db89c2687238071e927
deka014 <deepjyotideka8@gmail.com>
2022-08-22 18:48:41 +0530

    4sum using bs and 3 pointer

--- Day-15/Array/4Sum.py
+++ Day-15/Array/4Sum.py
@@ -37,6 +37,8 @@ class Solution:
         # or 
         # 2 pointer with left and right --> it will not create duplicate
         
+
+        # the below Solution is not handeling duplicates
         def threePointerApproach():
             output = []
             i = 0
@@ -65,6 +67,56 @@ class Solution:
                     j+=1
                 i+=1 
             return list(set(map(tuple,output) ))
+                  
+                        
+        
+        return threePointerApproach()
+
+
+        # the below Solution is by handeling duplicates
+
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        
+        
+        # this can be solved using three pointer with binary search which may create duplicates so set
+        # needs to be used 
+        # or 
+        # 2 pointer with left and right --> it will not create duplicate
+        
+        def threePointerApproach():
+            output = []
+            i = 0
+            N = len(nums)
+            nums.sort()
+            
+            while i < N :
+                j = i+1
+                while j < N :
+                    k=j+1
+                    while k < N :
+                        rem = target - (nums[i] + nums[j] + nums[k])
+                        l = k+1
+                        r = N-1
+                        while l <= r :
+                            mid = (l+r) // 2
+                            if nums[mid] == rem :
+                                output.append([nums[i],nums[j],nums[k],nums[mid]])
+                                break
+                            elif nums[mid] < rem :
+                                l = mid + 1
+                            else :
+                                r = mid - 1
+                        while k < N-1 and nums[k+1] == nums[k]:
+                            k+=1
+                        k+=1 
+                    while j < N-1 and nums[j+1] == nums[j]:
+                            j+=1
+                    j+=1
+                while i < N-1 and nums[i+1] == nums[i]:
+                            i+=1
+                i+=1 
+            return output
                   
                         
         
