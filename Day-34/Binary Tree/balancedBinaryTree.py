768e9e281880f958f925f7090e269de618d4a22f
deka014 <deepjyotideka8@gmail.com>
2023-02-09 01:39:06 +0530

    add balance binary tree question

---
+++ Day-34/Binary Tree/balancedBinaryTree.py
@@ -0,0 +1,61 @@
+# Given a binary tree, determine if it is
+# height-balanced
+# .
+
+ 
+
+# Example 1:
+
+# Input: root = [3,9,20,null,null,15,7]
+# Output: true
+
+# Example 2:
+
+# Input: root = [1,2,2,3,3,null,null,4,4]
+# Output: false
+
+# Example 3:
+
+# Input: root = []
+# Output: true
+
+ 
+
+# Constraints:
+
+#     The number of nodes in the tree is in the range [0, 5000].
+#     -104 <= Node.val <= 104
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root):
+
+        if root is None :
+            return True
+
+        ans = True
+
+        def rec(root):
+            
+            if root is None :
+                return 0 
+            
+            lh = rec(root.left)
+            rh = rec(root.right)
+
+            if lh == -1 or  rh == -1 or abs(lh-rh) > 1 :
+                return -1
+            
+            return max(lh,rh) + 1
+
+
+        return ans if rec(root) != -1 else False
+            
+
+
+
