59d85fad7eb2c6cf4a7595e41fae33eafa366f2b
deka014 <deepjyotideka8@gmail.com>
2023-02-10 03:00:14 +0530

    add boundary traversal question inution

---
+++ Day-35/boundaryTraversal.py
@@ -0,0 +1,18 @@
+# #intuiton 
+# https://www.codingninjas.com/codestudio/problems/boundary-traversal_790725?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website
+
+# https://takeuforward.org/data-structure/boundary-traversal-of-a-binary-tree/
+
+
+#inution
+
+# for this we need 3 function 
+
+# the first one to traverse the left boundary of the tree where we will check if leaf node .. if not then go to left else right 
+
+# the second one is leaf node function where we will check if the node is leaf node or not by using pre order traversal 
+
+# the third one is right node function where we will check if the node is leaf node .... if it is not then go to right else go to left then reverse the list 
+
+
+
