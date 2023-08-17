8ac8acbb4ae33123d008c72d0cacd4ae54cef268
deka014 <deepjyotideka8@gmail.com>
2023-08-17 23:22:52 +0530

    add new leetcode solutions

---
+++ Day-75/slidingWinMax.py
@@ -0,0 +1,54 @@
+# 239. Sliding Window Maximum
+# Hard
+# 16.7K
+# 561
+# Companies
+
+# You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+
+# Return the max sliding window.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+# Output: [3,3,5,5,6,7]
+# Explanation: 
+# Window position                Max
+# ---------------               -----
+# [1  3  -1] -3  5  3  6  7       3
+#  1 [3  -1  -3] 5  3  6  7       3
+#  1  3 [-1  -3  5] 3  6  7       5
+#  1  3  -1 [-3  5  3] 6  7       5
+#  1  3  -1  -3 [5  3  6] 7       6
+#  1  3  -1  -3  5 [3  6  7]      7
+
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        q  = []
+        output = []
+
+        l = 0 
+        r = 0 
+
+        while r < len(nums):
+
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            
+            q.append(r)
+
+            if l > q[0]:
+                q.pop(0)
+            
+            if r+1 >= k:
+                output.append(nums[q[0]])
+                l+=1
+            r+=1
+
+
+        return output
+
+        
