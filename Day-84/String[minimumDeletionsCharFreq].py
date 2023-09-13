337589ea736d865448f532a0c230179e092544d5
deka014 <deepjyotideka8@gmail.com>
2023-09-13 23:14:37 +0530

    add new string solution

---
+++ Day-84/String[minimumDeletionsCharFreq].py
@@ -0,0 +1,79 @@
+# 1647. Minimum Deletions to Make Character Frequencies Unique
+# Medium
+# 4.7K
+# 70
+# Companies
+
+# A string s is called good if there are no two different characters in s that have the same frequency.
+
+# Given a string s, return the minimum number of characters you need to delete to make s good.
+
+# The frequency of a character in a string is the number of times it appears in the string. For example, in the string "aab", the frequency of 'a' is 2, while the frequency of 'b' is 1.
+
+ 
+
+# Example 1:
+
+# Input: s = "aab"
+# Output: 0
+# Explanation: s is already good.
+
+# Example 2:
+
+# Input: s = "aaabbbcc"
+# Output: 2
+# Explanation: You can delete two 'b's resulting in the good string "aaabcc".
+# Another way it to delete one 'b' and one 'c' resulting in the good string "aaabbc".
+
+# Example 3:
+
+# Input: s = "ceabaacb"
+# Output: 2
+# Explanation: You can delete both 'c's resulting in the good string "eabaab".
+# Note that we only care about characters that are still in the string at the end (i.e. frequency of 0 is ignored).
+
+ 
+
+# Constraints:
+
+#     1 <= s.length <= 105
+#     s contains only lowercase English letters.
+
+
+class Solution:
+    def minDeletions(self, s: str) -> int:
+
+        smap = {}
+        for letter in s :
+            if letter in smap :
+                smap[letter] += 1
+            else :
+                smap[letter] = 1
+            
+        foundFreq = set([])
+        tobeadjusted = []
+        newfreq = set([])
+
+        for key in smap :
+            if smap[key] in foundFreq :
+                tobeadjusted.append(smap[key])
+            else :
+                foundFreq.add(smap[key])
+        
+        if not tobeadjusted :
+            return 0
+
+        ans = 0
+
+        for freq in tobeadjusted :
+            while freq > 0 and freq in foundFreq :
+                freq-=1
+                ans+=1
+            foundFreq.add(freq)
+
+        return ans
+
+
+        return ans
+
+    
