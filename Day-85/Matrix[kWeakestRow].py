c32f746f00de95e1400d271ac4b99893f3fdefdb
deka014 <deepjyotideka8@gmail.com>
2023-09-18 22:53:25 +0530

    add new matrix easy solution

---
+++ Day-85/Matrix[kWeakestRow].py
@@ -0,0 +1,102 @@
+# 1337. The K Weakest Rows in a Matrix
+# Easy
+# 3.9K
+# 218
+# Companies
+
+# You are given an m x n binary matrix mat of 1's (representing soldiers) and 0's (representing civilians). The soldiers are positioned in front of the civilians. That is, all the 1's will appear to the left of all the 0's in each row.
+
+# A row i is weaker than a row j if one of the following is true:
+
+#     The number of soldiers in row i is less than the number of soldiers in row j.
+#     Both rows have the same number of soldiers and i < j.
+
+# Return the indices of the k weakest rows in the matrix ordered from weakest to strongest.
+
+ 
+
+# Example 1:
+
+# Input: mat = 
+# [[1,1,0,0,0],
+#  [1,1,1,1,0],
+#  [1,0,0,0,0],
+#  [1,1,0,0,0],
+#  [1,1,1,1,1]], 
+# k = 3
+# Output: [2,0,3]
+# Explanation: 
+# The number of soldiers in each row is: 
+# - Row 0: 2 
+# - Row 1: 4 
+# - Row 2: 1 
+# - Row 3: 2 
+# - Row 4: 5 
+# The rows ordered from weakest to strongest are [2,0,3,1,4].
+
+# Example 2:
+
+# Input: mat = 
+# [[1,0,0,0],
+#  [1,1,1,1],
+#  [1,0,0,0],
+#  [1,0,0,0]], 
+# k = 2
+# Output: [0,2]
+# Explanation: 
+# The number of soldiers in each row is: 
+# - Row 0: 1 
+# - Row 1: 4 
+# - Row 2: 1 
+# - Row 3: 1 
+# The rows ordered from weakest to strongest are [0,2,3,1].
+
+ 
+
+# Constraints:
+
+#     m == mat.length
+#     n == mat[i].length
+#     2 <= n, m <= 100
+#     1 <= k <= m
+#     matrix[i][j] is either 0 or 1.
+
+# Accepted
+# 314.6K
+# Submissions
+# 430.2K
+# Acceptance Rate
+# 73.1%
+
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        
+        row = len(mat)
+        col = len(mat[0])
+
+        ordermap = [[] for _ in range(col+1)]
+        ans = []
+
+        for i in range(row):
+            count = 0
+            for j in range(col):
+
+                if mat[i][j] == 0 :
+                    break
+                
+                count+=1
+            
+            ordermap[count].append(i)
+                
+
+        
+        for i in range(len(ordermap)):
+            if ordermap[i] :
+                for num in ordermap[i]:
+                    ans.append(num)
+
+                    if len(ans) == k :
+                        return ans
+
+
+
