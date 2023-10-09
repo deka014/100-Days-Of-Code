f1bf11d5684e6ddfe9dca40fdc5b5500b05492c9
deka014 <deepjyotideka8@gmail.com>
2023-10-09 21:28:55 +0530

    add dp hard solution

--- Day-90/[DP]maxDotProduct.py
+++ Day-90/[DP]maxDotProduct.py
@@ -1,71 +1,29 @@
-# 1458. Max Dot Product of Two Subsequences
-# Hard
-# 1.4K
-# 27
-# Companies
-
-# Given two arrays nums1 and nums2.
-
-# Return the maximum dot product between non-empty subsequences of nums1 and nums2 with the same length.
-
-# A subsequence of a array is a new array which is formed from the original array by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, [2,3,5] is a subsequence of [1,2,3,4,5] while [1,5,3] is not).
-
- 
-
-# Example 1:
-
-# Input: nums1 = [2,1,-2,5], nums2 = [3,0,-6]
-# Output: 18
-# Explanation: Take subsequence [2,-2] from nums1 and subsequence [3,-6] from nums2.
-# Their dot product is (2*3 + (-2)*(-6)) = 18.
-
-# Example 2:
-
-# Input: nums1 = [3,-2], nums2 = [2,-6,7]
-# Output: 21
-# Explanation: Take subsequence [3] from nums1 and subsequence [7] from nums2.
-# Their dot product is (3*7) = 21.
-
-# Example 3:
-
-# Input: nums1 = [-1,-1], nums2 = [1,1]
-# Output: -1
-# Explanation: Take subsequence [-1] from nums1 and subsequence [1] from nums2.
-# Their dot product is -1.
-
- 
-
-# Constraints:
-
-#     1 <= nums1.length, nums2.length <= 500
-#     -1000 <= nums1[i], nums2[i] <= 1000
-
 class Solution:
     def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
 
-        if len(nums2) > len(nums1) :
-            nums1 , nums2 = nums2 , nums1
+        n1 = len(nums1)
+        n2 = len(nums2)
+        dp = {}
 
-        ans = [min(nums1) * min(nums2)]
+        def rec(ind1,ind2):
 
-        def rec(ind1,ind2,total):
+            if ind1 >= n1 or ind2 >= n2 :
+                return -10**9
 
-            if ind1 == len(nums1) or ind2 == len(nums2) :
-                ans[0] = max(ans[0],total)
-                print(total)
-                return
+            if (ind1,ind2) in dp :
+                return dp[(ind1,ind2)]
 
+            val = nums1[ind1] * nums2[ind2]
 
-            for i in range(ind2+1,len(nums2)):
+            f1 = val + rec(ind1+1,ind2+1)
 
-                if (nums1[ind1] > 0 and nums2[ind2] > 0) or (nums1[ind1] < 0 and nums2[ind2] < 0):
-                    print(nums1[ind1] , nums2[ind2])
-                    rec(ind1+1,i,total+ (nums1[ind1]*nums2[ind2]))
-                
-                rec(ind1+1,ind2,total)
-                
-            
+            f2 = rec(ind1+1,ind2)
 
-        rec(0,-1,0)
+            f3 = rec(ind1,ind2+1)
 
-        return ans[0]
+            dp[(ind1,ind2)] = max(val,f1,f2,f3)
+
+            return dp[(ind1,ind2)]
+
+        return rec(0,0)
+        
