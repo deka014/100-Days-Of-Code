419bc746aeb90328f0990b6a99f04e8093cebc6d
deka014 <deepjyotideka8@gmail.com>
2024-02-07 22:19:15 +0530

    add leetcode daily solution

--- Day-116/[String]firstUniqueCharacterInString.java
+++ Day-116/[String]firstUniqueCharacterInString.java
@@ -1,41 +1,44 @@
-# 387. First Unique Character in a String
-# Easy
-# 8.7K
-# 282
-# Companies
+import java.util.HashMap;
+import java.util.Map;
 
-# Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+// # 387. First Unique Character in a String
+// # Easy
+// # 8.7K
+// # 282
+// # Companies
 
- 
-
-# Example 1:
-
-# Input: s = "leetcode"
-# Output: 0
-
-# Example 2:
-
-# Input: s = "loveleetcode"
-# Output: 2
-
-# Example 3:
-
-# Input: s = "aabb"
-# Output: -1
+// # Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
 
  
 
-# Constraints:
+// # Example 1:
 
-#     1 <= s.length <= 105
-#     s consists of only lowercase English letters.
+// # Input: s = "leetcode"
+// # Output: 0
 
-# Accepted
-# 1.6M
-# Submissions
-# 2.6M
-# Acceptance Rate
-# 61.5%
+// # Example 2:
+
+// # Input: s = "loveleetcode"
+// # Output: 2
+
+// # Example 3:
+
+// # Input: s = "aabb"
+// # Output: -1
+
+ 
+
+// # Constraints:
+
+// #     1 <= s.length <= 105
+// #     s consists of only lowercase English letters.
+
+// # Accepted
+// # 1.6M
+// # Submissions
+// # 2.6M
+// # Acceptance Rate
+// # 61.5%
 
 class Solution {
     public int firstUniqChar(String s) {
